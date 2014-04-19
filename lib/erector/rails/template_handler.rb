332280d524a84d290a2520017637871aeaee5b2b
Scott Fleckenstein <nullstyle@gmail.com>
2014-04-19 13:27:21 -0700

    Modifies template handler to work with my style
    This commit changes the erector template handler to work with nesting
    your templates underneath the app/views directory.  This lets us avoid
    the grossness of adding /app to your autoload path.  In my case, I have
    app/views/pages, app/views/layouts, etc.

--- lib/erector/rails/template_handler.rb
+++ lib/erector/rails/template_handler.rb
@@ -3,11 +3,26 @@ module Erector
     class TemplateHandler
       def call(template)
         require_dependency template.identifier
-        widget_class_name = "views/#{template.identifier =~ %r(views/([^.]*)(\..*)?\.rb) && $1}".camelize
         is_partial = File.basename(template.identifier) =~ /^_/
         <<-SRC
-        Erector::Rails.render(#{widget_class_name}, self, local_assigns, #{!!is_partial})
+        Erector::Rails.render(#{widget_class_name(template.identifier)}, self, local_assigns, #{!!is_partial})
         SRC
+      end
+
+      private
+      def widget_class_name(identifier)
+        prefix = view_paths.find{ |prefix| identifier.starts_with? prefix }
+        raise ArgumentError, "Template is not underneath rails view_paths" if prefix.blank?
+
+        # remove leading slash and trailing .rb
+        stripped       = identifier.sub(prefix, "")[1..-4]
+        view_path_base = File.basename(prefix)
+
+        "#{view_path_base}/#{stripped}".camelize
+      end
+
+      def view_paths
+        ::Rails.configuration.action_controller.view_paths
       end
     end
   end
